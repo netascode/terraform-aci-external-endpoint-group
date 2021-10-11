370f850213c01cdcc892f35c05057251055e36e7
danischm <danischm@cisco.com>
2021-10-11 07:19:37 +0200

    Update implicit resource dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+- Update implicit resource dependencies
+
 ## 0.0.1
 
 - Initial release
--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aci_rest" "l3extInstP" {
 
 resource "aci_rest" "fvRsCons" {
   for_each   = toset(var.contract_consumers)
-  dn         = "${aci_rest.l3extInstP.id}/rscons-${each.value}"
+  dn         = "${aci_rest.l3extInstP.dn}/rscons-${each.value}"
   class_name = "fvRsCons"
   content = {
     tnVzBrCPName = each.value
@@ -28,7 +28,7 @@ resource "aci_rest" "fvRsCons" {
 
 resource "aci_rest" "fvRsProv" {
   for_each   = toset(var.contract_providers)
-  dn         = "${aci_rest.l3extInstP.id}/rsprov-${each.value}"
+  dn         = "${aci_rest.l3extInstP.dn}/rsprov-${each.value}"
   class_name = "fvRsProv"
   content = {
     tnVzBrCPName = each.value
@@ -37,7 +37,7 @@ resource "aci_rest" "fvRsProv" {
 
 resource "aci_rest" "fvRsConsIf" {
   for_each   = toset(var.contract_imported_consumers)
-  dn         = "${aci_rest.l3extInstP.id}/rsconsIf-${each.value}"
+  dn         = "${aci_rest.l3extInstP.dn}/rsconsIf-${each.value}"
   class_name = "fvRsConsIf"
   content = {
     tnVzCPIfName = each.value
@@ -46,7 +46,7 @@ resource "aci_rest" "fvRsConsIf" {
 
 resource "aci_rest" "l3extSubnet" {
   for_each   = { for subnet in var.subnets : subnet.prefix => subnet }
-  dn         = "${aci_rest.l3extInstP.id}/extsubnet-[${each.value.prefix}]"
+  dn         = "${aci_rest.l3extInstP.dn}/extsubnet-[${each.value.prefix}]"
   class_name = "l3extSubnet"
   content = {
     ip    = each.value.prefix
@@ -57,7 +57,7 @@ resource "aci_rest" "l3extSubnet" {
 
 resource "aci_rest" "l3extRsSubnetToRtSumm" {
   for_each   = { for subnet in var.subnets : subnet.prefix => subnet if subnet.bgp_route_summarization == true }
-  dn         = "${aci_rest.l3extSubnet[each.value.prefix].id}/rsSubnetToRtSumm"
+  dn         = "${aci_rest.l3extSubnet[each.value.prefix].dn}/rsSubnetToRtSumm"
   class_name = "l3extRsSubnetToRtSumm"
   content = {
     tDn = "uni/tn-common/bgprtsum-default"
